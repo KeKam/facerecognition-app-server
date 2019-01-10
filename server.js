efc6a066b47e082a79a7835a72b10947fda79748
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-01-10 14:33:01 +0100

    Add CORS and fix signin endpoint

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
+
 const database = {
   users: [
     {
@@ -31,12 +34,11 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {
   if (req.body.email === database.users[0].email && 
-    req.body.password === database.users[0].password) {
-      res.json('success');
+      req.body.password === database.users[0].password) {
+    res.json('success');
     } else {
       res.status(400).json('error logging in');
     }
-  res.json('signed in');
 });
 
 app.post('/register', (req, res) => {
