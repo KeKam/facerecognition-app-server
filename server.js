96ab6996f1055895388b929b545ba922f44d0d6b
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-01-29 14:32:29 +0100

    Connect profile endpoint with database

--- server.js
+++ server.js
@@ -73,16 +73,15 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    } 
+  db.select('*').from('users').where({ id })
+    .then(user => {
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json('Not found');
+    }
   })
-  if (!found) {
-    res.status(400).json('Not found');
-  }
+  .catch(err => res.status(400).json('Error getting user'))
 });
 
 app.put('/image', (req, res) => {
