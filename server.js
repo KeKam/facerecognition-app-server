0dd12bd9afcd4be9fd624348f756aff52d34cc23
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2019-02-12 14:55:00 +0100

    Setup database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'KeKam',
-    password : process.env.DATABASE_PASSWORD,
-    database : 'facerecognition'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
